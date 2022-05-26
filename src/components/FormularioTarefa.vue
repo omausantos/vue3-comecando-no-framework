accea369cd2f7216a77609f4a20c36148ee9bce9
Mauricio Santos <mauwebjc@gmail.com>
2022-05-26 19:22:02 -0300

    [A2] Evoluindo o temporizador

--- src/components/FormularioTarefa.vue
+++ src/components/FormularioTarefa.vue
@@ -5,21 +5,7 @@
                 <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
             </div>
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
-                    <button class="button" @click="iniciar">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="finalizar">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
-                </div>
+                <Temporizador />
             </div>
         </div>
     </div>
@@ -27,28 +13,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Cronometro from './Cronometro.vue';
+import Temporizador from './Temporizador.vue';
 
 export default defineComponent({
     name: 'FormularioTarefa',
-    data() {
-        return {
-            tempoEmSegundos: 0,
-            cronometro: 0,
-        }
-    },
     components: {
-        Cronometro
+        Temporizador
     },
-    methods: {
-        iniciar(): void {
-            this.cronometro = setInterval(() => {
-                this.tempoEmSegundos++
-            }, 1000);
-        },
-        finalizar(): void {
-            clearInterval(this.cronometro);
-        }
-    }
 })
 </script>
