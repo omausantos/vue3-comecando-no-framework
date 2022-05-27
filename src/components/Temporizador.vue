abf9981eb6ed8551811c97247576f35a79c15bb4
Mauricio Santos <mauwebjc@gmail.com>
2022-05-26 21:01:34 -0300

    [A2] Desafio

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,24 +1,15 @@
 <template>
     <div class="is-flex is-align-items-center is-justify-content-space-between">
         <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-        <button class="button" @click="iniciar" :disabled="cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-        </button>
-        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-        </button>
+        <TemporizadorBotao icone="fa-play" :cronometroRodando="cronometroRodando" textoBotao="play" @aoClicar="iniciar" />
+        <TemporizadorBotao icone="fa-stop" :cronometroRodando="!cronometroRodando" textoBotao="parar" @aoClicar="finalizar" />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Cronometro from './Cronometro.vue';
+import TemporizadorBotao from './TemporizadorBotao.vue';
 
 export default defineComponent({
     name: 'Temporizador',
@@ -31,8 +22,9 @@ export default defineComponent({
         }
     },
     components: {
-        Cronometro
-    },
+    Cronometro,
+    TemporizadorBotao
+},
     methods: {
         iniciar(): void {
             this.cronometroRodando = true,
---
+++ src/components/TemporizadorBotao.vue
@@ -0,0 +1,36 @@
+<template>
+    <button class="button" @click="aoClicar" :disabled="cronometroRodando">
+        <span class="icon">
+            <i class="fas" :class="icone"></i>
+        </span>
+        <span>{{ textoBotao }}</span>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'TemporizadorBotao',
+    props: {
+        textoBotao: {
+            type: String,
+            default: ''
+        },
+        icone: {
+            type: String,
+            default: ''
+        },
+        cronometroRodando: {
+            type: Boolean,
+            default: false
+        }
+    },
+    emits: ['aoClicar'],
+    methods: {
+        aoClicar(): void {
+            this.$emit('aoClicar', null);
+        }
+    }
+})
+</script>
